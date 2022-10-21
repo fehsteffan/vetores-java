01fe4292b093aeac70e165473ec09c4a2d545a38
Felipe Steffan <felip.developer@hotmail.com>
2022-10-20 23:09:42 -0300

    Exercicio vetor II

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,6 @@
 package application;
 
-import entities.Product;
+
 
 import java.util.Locale;
 import java.util.Scanner;
@@ -13,23 +13,20 @@ public class Program {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
-        Product[] vect = new Product[n];
+        double[] vect = new double[n];
 
         for (int i=0; i<n; i++) {
-            sc.nextLine();
-            String name = sc.nextLine();
-            double price = sc.nextDouble();
-            vect[i] = new Product(name, price);
+            vect[i] = sc.nextDouble();
         }
         double sum = 0.0;
         for (int i=0; i<n; i++) {
-         sum += vect[i].getPrice();
+            sum += vect[i];
+
         }
         double avg = sum / n;
-        System.out.printf("AVARAGE PRICE = %.2f%n", avg);
-
-      }
-
+        System.out.printf(" MEDIA : %.2f%n " , avg);
     }
+
+}
 
 
